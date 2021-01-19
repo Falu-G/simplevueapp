5dc661daf099cb33ab45059fe6203bf57e7ce448
Falu-G <sfalugba@gmail.com>
2021-01-19 13:53:43 +0000

    myappupdate

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Falu-G Vue.js App"/>
   </div>
 </template>
 
